2c9cf07981372ec437cd5f477a795b44d071046c
danbrun <5322676-danbrun@users.noreply.gitlab.com>
2020-03-01 20:44:24 +0100

    init Java code

---
+++ textAdventureGame/src/textAdventureGame/Main.java
@@ -0,0 +1,26 @@
+package textAdventureGame;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	Parser gameParser;
+	Player gamePlayer;
+	
+	Main(){
+		gameParser = new Parser();
+		gamePlayer = new Player();
+	}
+	
+	void startGame() {
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			gameParser.parse(scanner.nextLine());
+		}
+	}
+	
+	public static void main(String args[]) {
+		new Main().startGame();
+	}
+
+}
---
+++ textAdventureGame/src/textAdventureGame/Parser.java
@@ -0,0 +1,53 @@
+package textAdventureGame;
+
+import java.util.Scanner;
+
+public class Parser {
+
+	public void parse(String string) {
+		Scanner commandScanner = new Scanner(string);
+		if (commandScanner.hasNext()) {
+			String command = commandScanner.next();
+			String arg;
+			
+			if (!commandScanner.hasNext()) {
+				System.out.println("what do you want to " + command + "?");
+				return;
+			} else {
+				arg = commandScanner.nextLine().trim().replaceAll(" +", " ");
+			}
+			
+			switch (command) {
+			case "move":
+				move(arg);
+				break;
+			case "kill":
+				kill(arg);
+				break;
+			case "use":
+				use(arg);
+				break;
+			default:
+				System.out.println("command not recongnised");
+			}
+			
+		} else {
+			System.out.println("command expected");
+			return;
+		}
+		
+	}
+	
+	private void move(String arg) {
+		System.out.println("moved " + arg);
+	}
+	
+	private void kill(String arg) {
+		System.out.println(arg + " was killed");
+	}
+	
+	private void use(String arg) {
+		System.out.println(arg + " was used");
+	}
+	
+}
