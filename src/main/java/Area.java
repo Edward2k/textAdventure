aafd4a1b1fb6297f262c0df911117db730d8d220
debrunet <61621711+debrunet@users.noreply.github.com>
2020-03-04 09:43:52 +0100

    Update Area.java

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -4,9 +4,22 @@ public class Area {
     private Item obstacle;
     private boolean obstacleNeutralized;
     private String name;
+    private String description;
 
     Area () {
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean canEnter() {
+        return obstacleNeutralized;
     }
 
 }
